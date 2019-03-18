52826ccc6bf270d918d0f0e3c777250a4d2014d2
2298618484 <48661983+2298618484@users.noreply.github.com>
2019-03-18 15:47:53 +0800

    Update test.java

--- src/main/java/com/hr/sb5mybatis/controller/test.java
+++ src/main/java/com/hr/sb5mybatis/controller/test.java
@@ -20,12 +20,7 @@ public class test {
 
     @RequestMapping("/queryAll")
     public ModelAndView queryAll(){
-        System.out.println("222");
-        System.out.println("111");
-        System.out.println("666666666");
-         System.out.println("222");
-        System.out.println("111");
-        System.out.println("666666666");
+       
         List<Person> list = ps.queryAll();
         ModelAndView view = new ModelAndView("/list");
         view.addObject("list",list);
